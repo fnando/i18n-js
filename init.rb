76d17b1b7daf65faf0d40ced447f86ef644f3c53
Nando Vieira <fnando.vieira@gmail.com>
2009-10-29 13:25:19 -0200

    Rescuing export method.

--- init.rb
+++ init.rb
@@ -1,5 +1,8 @@
 require File.dirname(__FILE__) + "/lib/i18n-js"
 
 Rails.configuration.after_initialize do
-  SimplesIdeias::I18n.export!
+  begin
+    SimplesIdeias::I18n.export!
+  rescue Exception
+  end
 end
