8a0f739ae31d45789cf9b9e22f674781df26eec7
Sebastien Grosjean <public@zencocoon.com>
2009-11-13 23:16:18 +0200

    Add tests basics, I18n stubing missing + complete test suits

--- test/i18n-js_test.rb
+++ test/i18n-js_test.rb
@@ -2,31 +2,31 @@ require 'test_helper'
 
 class I18nJSTest < ActiveSupport::TestCase
   setup do
-    # `rm #{SimplesIdeias::I18n::JAVASCRIPT_DIR}/{messages,i18n}.js 2>&1 /dev/null`
+    # Remove temporary directory if already present
+    FileUtils.rm_r(RAILS_ROOT) if File.exist?(RAILS_ROOT)
+
+    # Create temporary directory to test the files generation
+    FileUtils.mkdir_p([RAILS_ROOT+"/config", RAILS_ROOT+"/public/javascripts/"])
+
+    # Load test config file
+    @config = YAML.load(File.open(File.dirname(__FILE__) + '/fixtures/i18n-js.yml'))
   end
   
   teardown do
-    # `rm #{SimplesIdeias::I18n::JAVASCRIPT_DIR}/{messages,i18n}.js 2>&1 /dev/null`
+    # Remove temporary directory
+    FileUtils.rm_r(RAILS_ROOT)
+  end
+
+  test "export! : copy config if not found" do
+    assert !File.exist?(::SimplesIdeias::I18n::CONFIG_FILE)
+    SimplesIdeias::I18n.export!
+    assert File.exist?(::SimplesIdeias::I18n::CONFIG_FILE)
+  end
+
+  test "export! : export messages files" do
+    # FIXME: Stub I18n to depend on fixtures/locales.yml
+    SimplesIdeias::I18n.export!(@config)
   end
   
-  # test "copy i18n.js" do
-  #   SimplesIdeias::I18n.copy!
-  #   assert File.exist?(SimplesIdeias::I18n::JAVASCRIPT_DIR + "/i18n.js")
-  # end
-  #
-  # test "export messages.js" do
-  #   SimplesIdeias::I18n.export!
-  #   assert File.exist?(SimplesIdeias::I18n::JAVASCRIPT_DIR + "/messages.js")
-  # end
-  #
-  # test "rake should generate file" do
-  #   `cd #{Rails.root} && rake i18n:generate`
-  #   assert File.exist?(SimplesIdeias::I18n::JAVASCRIPT_DIR + "/messages.js")
-  # end
-  #
-  # test "rake should copy & generate files" do
-  #   `cd #{Rails.root} && rake i18n:setup`
-  #   assert File.exist?(SimplesIdeias::I18n::JAVASCRIPT_DIR + "/messages.js")
-  #   assert File.exist?(SimplesIdeias::I18n::JAVASCRIPT_DIR + "/i18n.js")
-  # end
+  # FIXME: Add rest of the tests
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,9 @@ require 'test/unit'
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
+
+# Stub RAILS_ROOT to get proper files writing location in every environments
+# Keep really careful with this files path as this folder is going to be removed !!!
+RAILS_ROOT = File.dirname(__FILE__) + "/tmp"
+
+require File.dirname(__FILE__) + '/../lib/i18n-js'
