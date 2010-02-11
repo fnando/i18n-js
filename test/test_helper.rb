9891f85c99b0f28211503568658f28dad3ce3989
Sebastien Grosjean <public@zencocoon.com>
2010-02-11 22:30:48 +0200

    Add dependency

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require 'test/unit'
 require 'active_support'
 require 'active_support/test_case'
 require 'ostruct'
+require 'pathname'
 
 # Stub Rails.root, so we don't need to load the whole Rails environment.
 # Be careful! The specified folder will be removed!
