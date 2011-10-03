d135d94c655ad9138a33ffb1962d48353637df1a
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-10-03 15:16:34 +0100

    explicitly write cache file if it doesn't exist - can happen under some asset:precompile task conditions

--- lib/i18n-js/engine.rb
+++ lib/i18n-js/engine.rb
@@ -6,13 +6,18 @@ module SimplesIdeias
 
       initializer 'i18n-js.asset_dependencies', :after => "sprockets.environment" do
         config = Rails.root.join('config','i18n-js.yml')
+        cache_file = I18n::Engine.load_path_hash_cache
         Rails.application.assets.register_preprocessor 'application/javascript', :'i18n-js_dependencies' do |ctx,data|
           if ctx.logical_path == I18N_TRANSLATIONS_ASSET
             ctx.depend_on(config)
             # also set up dependencies on every locale file
             ::I18n.load_path.each {|path| ctx.depend_on(path)}
-            # set up a dependency on the contents of the load path itself
-            ctx.depend_on(I18n::Engine.load_path_hash_cache)
+            # Set up a dependency on the contents of the load path
+            # itself.  In some situations it is possible to get here
+            # before the path hash cache file has been written; in
+            # this situation, write it now.
+            I18n::Engine.write_hash! unless File.exists?(cache_file)
+            ctx.depend_on(cache_file)
           end
           data
         end
@@ -33,11 +38,15 @@ module SimplesIdeias
         # Rewrite path cache when it changes
         def write_hash_if_changed
           load_path_hash = ::I18n.load_path.hash
-          if load_path_hash != self.cached_load_path_hash
+          if load_path_hash != cached_load_path_hash
             self.cached_load_path_hash = load_path_hash
-            File.open(load_path_hash_cache,'w') do |f| 
-              f.write(load_path_hash)
-            end
+            write_hash!
+          end
+        end
+        
+        def write_hash!
+          File.open(load_path_hash_cache,'w') do |f| 
+            f.write(cached_load_path_hash)
           end
         end
       end
