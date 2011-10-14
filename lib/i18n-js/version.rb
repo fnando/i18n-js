f9f42cba4bf258af2d6187a5fc23aceb32236df4
Nando Vieira <fnando.vieira@gmail.com>
2011-10-14 12:21:15 -0300

    Bump up version

--- lib/i18n-js/version.rb
+++ lib/i18n-js/version.rb
@@ -2,8 +2,8 @@ module SimplesIdeias
   module I18n
     module Version
       MAJOR = 2
-      MINOR = 0
-      PATCH = 1
+      MINOR = 1
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
   end
