8cb22f5d2bbef7014a1d1406f908b5fa4c5b7cca
Nando Vieira <fnando.vieira@gmail.com>
2011-11-17 08:07:04 -0200

    Removed some comments.

--- lib/i18n-js/middleware.rb
+++ lib/i18n-js/middleware.rb
@@ -31,24 +31,23 @@ module SimplesIdeias
         changed_files = []
 
         valid_cache.push cache_path.exist?
-        valid_cache.push ::I18n.load_path.uniq.size == cache.size # use uniq because load_path may have duplicated values
+        valid_cache.push ::I18n.load_path.uniq.size == cache.size
 
         ::I18n.load_path.each do |path|
           change = File.mtime(path).to_i
           file_has_changed = change != cache[path]
           valid_cache.push file_has_changed
-          valid_cache.push !file_has_changed # push true if the file has NOT changed
+          valid_cache.push !file_has_changed
           changed_files << path if file_has_changed
           cache[path] = change
         end
 
-        # Only write to the cache and export the translations if the cache has changed
         unless valid_cache.all?
           File.open(cache_path, "w+") do |file|
-            file << cache.to_yaml           
-          end          
+            file << cache.to_yaml
+          end
 
-          SimplesIdeias::I18n.export!     
+          SimplesIdeias::I18n.export!
         end
       end
     end
