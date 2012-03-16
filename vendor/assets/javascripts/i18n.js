eeec14066b50f72876aa789c9b1f226172452798
Nando Vieira <fnando.vieira@gmail.com>
2012-03-16 08:33:55 -0300

    Wrap one-line if into {}.

--- vendor/assets/javascripts/i18n.js
+++ vendor/assets/javascripts/i18n.js
@@ -3,34 +3,41 @@ if (!Array.prototype.indexOf) {
   Array.prototype.indexOf = function(searchElement /*, fromIndex */) {
     "use strict";
 
-    if (this === void 0 || this === null)
+    if (this === void 0 || this === null) {
       throw new TypeError();
+    }
 
     var t = Object(this);
     var len = t.length >>> 0;
-    if (len === 0)
+
+    if (len === 0) {
       return -1;
+    }
 
     var n = 0;
     if (arguments.length > 0) {
       n = Number(arguments[1]);
-      if (n !== n) // shortcut for verifying if it's NaN
+      if (n !== n) { // shortcut for verifying if it's NaN
         n = 0;
-      else if (n !== 0 && n !== (Infinity) && n !== -(Infinity))
+      } else if (n !== 0 && n !== (Infinity) && n !== -(Infinity)) {
         n = (n > 0 || -1) * Math.floor(Math.abs(n));
+      }
     }
 
-    if (n >= len)
+    if (n >= len) {
       return -1;
+    }
 
     var k = n >= 0
           ? n
           : Math.max(len - Math.abs(n), 0);
 
     for (; k < len; k++) {
-      if (k in t && t[k] === searchElement)
+      if (k in t && t[k] === searchElement) {
         return k;
+      }
     }
+
     return -1;
   };
 }
