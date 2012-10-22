64222cd858e31e7b6b6a7d15f25b4b3eef091c33
Patrick Helm <deradon87@gmail.com>
2012-10-22 22:38:13 +0200

    Loads i18n-js.yml as erb file

--- lib/i18n-js.rb
+++ lib/i18n-js.rb
@@ -90,7 +90,8 @@ module SimplesIdeias
     # custom output directory
     def config
       if config?
-        (YAML.load_file(config_file) || {}).with_indifferent_access
+        erb = ERB.new(File.read(config_file)).result
+        (YAML.load(erb) || {}).with_indifferent_access
       else
         {}
       end
