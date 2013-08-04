4e5c525ff6e1ec4d3449852746fa5651a0577d68
Nando Vieira <fnando.vieira@gmail.com>
2013-08-04 14:07:29 -0300

    Rails 4.

--- lib/i18n/js/engine.rb
+++ lib/i18n/js/engine.rb
@@ -8,7 +8,9 @@ module I18n
           next unless JS.has_asset_pipeline?
           next unless Rails.configuration.assets.compile
 
-          Rails.application.assets.register_preprocessor "application/javascript", :"i18n-js_dependencies" do |context, source|
+          registry = Sprockets.respond_to?("register_preprocessor") ? Sprockets : Rails.application.assets
+
+          registry.register_preprocessor "application/javascript", :"i18n-js_dependencies" do |context, source|
             next source unless context.logical_path == "i18n/filtered"
             ::I18n.load_path.each {|path| context.depend_on(File.expand_path(path))}
             source
