7a5ed4795cf4f8f07b10cb4ab3419f6f558b253f
Krzysztof Herod <krzysztof.herod@xing.com>
2014-04-24 14:23:14 +0200

    do not ignore fractions of a second

--- spec/js/dates.spec.js
+++ spec/js/dates.spec.js
@@ -58,6 +58,11 @@ describe("Dates", function(){
     expected = new Date(Date.UTC(2009, 0, 24, 15, 33, 44));
     actual = I18n.parseDate("2009-01-24T15:33:44Z");
     expect(actual.toString()).toEqual(expected.toString());
+
+    expected = new Date(Date.UTC(2009, 0, 24, 15, 34, 44, 200));
+    actual = I18n.parseDate("2009-01-24T15:34:44.200Z");
+    expect(actual.toString()).toEqual(expected.toString());
+    expect(actual.getMilliseconds()).toEqual(expected.getMilliseconds())
   });
 
   it("formats date", function(){
