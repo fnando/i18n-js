de1e869b128637f0e77613d7593f7e19fe58a818
Paweł Gościcki <pawel.goscicki@gmail.com>
2015-03-05 14:58:30 +0100

    Add .deep_reject method to JS::Utils
    It is necessary for the :except option (to deep reject keys from the
    translations hash that we don't want to include in the outputted
    translations).

--- lib/i18n/js/utils.rb
+++ lib/i18n/js/utils.rb
@@ -5,10 +5,10 @@ module I18n
       MERGER = proc do |key, v1, v2|
         Hash === v1 && Hash === v2 ? v1.merge(v2, &MERGER) : v2
       end
-      HASH_NIL_VALUE_CLEANER_PROC = proc do |k, v|
-         v.kind_of?(Hash) ? (v.delete_if(&HASH_NIL_VALUE_CLEANER_PROC); false) : v.nil?
-      end
 
+      HASH_NIL_VALUE_CLEANER_PROC = proc do |k, v|
+        v.kind_of?(Hash) ? (v.delete_if(&HASH_NIL_VALUE_CLEANER_PROC); false) : v.nil?
+      end
 
       def self.strip_keys_with_nil_values(hash)
         hash.dup.delete_if(&HASH_NIL_VALUE_CLEANER_PROC)
@@ -20,6 +20,14 @@ module I18n
 
       def self.deep_merge!(target_hash, hash) # :nodoc:
         target_hash.merge!(hash, &MERGER)
+      end
+
+      def self.deep_reject(hash, &block)
+        hash.each_with_object({}) do |(k, v), memo|
+          unless block.call(k, v)
+            memo[k] = v.kind_of?(Hash) ? deep_reject(v, &block) : v
+          end
+        end
       end
     end
   end
