3e0c1f112617c0110c41952652afd9e67273247c
Matt Jonker <matt@mattjonker.com>
2015-04-21 05:36:40 -0400

    Converted spec comparison to be against string instead of file contents

--- spec/i18n_js_spec.rb
+++ spec/i18n_js_spec.rb
@@ -488,9 +488,11 @@ EOS
         end
 
         it "exports with the keys sorted" do
-          correct_output = File.read(File.dirname(__FILE__) + "/fixtures/js_sort_translation_keys_true_output.js")
-
-          expect(subject).to eq correct_output
+          expect(subject).to eq(<<EOS
+I18n.translations || (I18n.translations = {});
+I18n.translations["en"] = {"admin":{"edit":{"title":"Edit"},"show":{"note":"more details","title":"Show"}},"date":{"abbr_day_names":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"abbr_month_names":[null,"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],"day_names":["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],"formats":{"default":"%Y-%m-%d","long":"%B %d, %Y","short":"%b %d"},"month_names":[null,"January","February","March","April","May","June","July","August","September","October","November","December"]},"fallback_test":"Success","foo":"Foo","number":{"currency":{"format":{"delimiter":",","format":"%u%n","precision":2,"separator":".","unit":"$"}},"format":{"delimiter":",","precision":3,"separator":"."}},"time":{"am":"am","formats":{"default":"%a, %d %b %Y %H:%M:%S %z","long":"%B %d, %Y %H:%M","short":"%d %b %H:%M"},"pm":"pm"}};
+EOS
+)
         end
       end
     end
