4178100bb85f03b09bf79558eb984efa250b8c83
PikachuEXE <pikachuexe@gmail.com>
2015-10-07 14:50:57 +0800

    ! Revert some changes from last pulled PR to fix ReferenceError in JS spec

--- app/assets/javascripts/i18n.js
+++ app/assets/javascripts/i18n.js
@@ -12,16 +12,19 @@
 // See tests for specific formatting like numbers and dates.
 //
 
-(function( global, factory ) {
-
-  if ( typeof module === "object" && typeof module.exports === "object" ) {
-    module.exports = factory(global);
+;(function(factory) {
+  if (typeof module !== 'undefined' && module.exports) {
+    // Node/CommonJS
+    module.exports = factory(this);
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    var global=this;
+    define('i18n', function(){ return factory(global);});
   } else {
-    factory( global );
+    // Browser globals
+    this.I18n = factory(this);
   }
-
-// Pass this if window is not defined yet
-}(typeof window !== "undefined" ? window : this, function( global, noGlobal ) {
+}(function(global) {
   "use strict";
 
   // Use previously defined object if exists in current scope
@@ -885,16 +888,6 @@
   I18n.t = I18n.translate;
   I18n.l = I18n.localize;
   I18n.p = I18n.pluralize;
-
-  if ( typeof define === "function" && define.amd ) {
-    define(function() {
-      return I18n;
-    });
-  }
-
-  if ( typeof noGlobal === typeof undefined ) {
-    window.I18n = I18n;
-  }
 
   return I18n;
 }));
