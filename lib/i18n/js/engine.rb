dce9149318afc789a2e94d145b4433778e244a8f
mchristen <mchristen@ownerscircle.com>
2015-12-08 15:45:25 -0800

    Fix for Sprockets 3.0 API change

--- lib/i18n/js/engine.rb
+++ lib/i18n/js/engine.rb
@@ -20,7 +20,9 @@ module I18n
         next unless JS::Dependencies.using_asset_pipeline?
         next unless JS::Dependencies.sprockets_supports_register_preprocessor?
 
-        Rails.application.assets.register_preprocessor "application/javascript", :"i18n-js_dependencies" do |context, source|
+        ##                    (sprockets 2.0 || sprockets 3.0)
+        sprockets = Rails.application.assets || Sprockets
+        sprockets.register_preprocessor "application/javascript", :"i18n-js_dependencies" do |context, source|
           if context.logical_path == "i18n/filtered"
             ::I18n.load_path.each {|path| context.depend_on(File.expand_path(path))}
           end
