83adce4cf61d369e1fc3349ff06a5aa08de9fc43
johnnyshields <johnny.shields@gmail.com>
2019-01-17 22:52:58 +0900

    Add/fix specs

--- spec/ruby/i18n/js_spec.rb
+++ spec/ruby/i18n/js_spec.rb
@@ -524,6 +524,26 @@ EOS
     end
   end
 
+  describe "merging plural keys" do
+    before do
+      stub_const('I18n::JS::DEFAULT_EXPORT_DIR_PATH', temp_path)
+      set_config "merge_plurals.yml"
+      I18n::JS.export
+      file_should_exist "merge_plurals.js"
+    end
+
+    subject do
+      File.read(File.join(I18n::JS.export_i18n_js_dir_path, "merge_plurals.js"))
+    end
+
+    it "should correctly merge the plural keys" do
+      expect(subject).to eq <<EOS
+I18n.translations || (I18n.translations = {});
+I18n.translations[\"en\"] = I18n.extend((I18n.translations[\"en\"] || {}), {\"merge_plurals\":{\"one\":\"Apple\",\"other\":\"Apples\"}});\nI18n.translations[\"fr\"] = I18n.extend((I18n.translations[\"fr\"] || {}), {\"merge_plurals\":{\"one\":\"Pomme\",\"other\":\"Pommes\",\"zero\":\"Pomme\"}});
+EOS
+    end
+  end
+
   describe "translation key sorting" do
 
     describe ".sort_translation_keys?" do
@@ -595,11 +615,10 @@ EOS
         end
 
         it "exports with the keys sorted" do
-          expect(subject).to eq(<<EOS
+          expect(subject).to eq <<EOS
 I18n.translations || (I18n.translations = {});
-I18n.translations["en"] = I18n.extend((I18n.translations["en"] || {}), {"admin":{"edit":{"title":"Edit"},"show":{"note":"more details","title":"Show"}},"date":{"abbr_day_names":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"abbr_month_names":[null,"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],"day_names":["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],"formats":{"default":"%Y-%m-%d","long":"%B %d, %Y","short":"%b %d"},"month_names":[null,"January","February","March","April","May","June","July","August","September","October","November","December"]},"fallback_test":"Success","foo":"Foo","null_test":"fallback for null","number":{"currency":{"format":{"delimiter":",","format":"%u%n","precision":2,"separator":".","unit":"$"}},"format":{"delimiter":",","precision":3,"separator":"."}},"time":{"am":"am","formats":{"default":"%a, %d %b %Y %H:%M:%S %z","long":"%B %d, %Y %H:%M","short":"%d %b %H:%M"},"pm":"pm"}});
+I18n.translations[\"en\"] = I18n.extend((I18n.translations[\"en\"] || {}), {\"admin\":{\"edit\":{\"title\":\"Edit\"},\"show\":{\"note\":\"more details\",\"title\":\"Show\"}},\"date\":{\"abbr_day_names\":[\"Sun\",\"Mon\",\"Tue\",\"Wed\",\"Thu\",\"Fri\",\"Sat\"],\"abbr_month_names\":[null,\"Jan\",\"Feb\",\"Mar\",\"Apr\",\"May\",\"Jun\",\"Jul\",\"Aug\",\"Sep\",\"Oct\",\"Nov\",\"Dec\"],\"day_names\":[\"Sunday\",\"Monday\",\"Tuesday\",\"Wednesday\",\"Thursday\",\"Friday\",\"Saturday\"],\"formats\":{\"default\":\"%Y-%m-%d\",\"long\":\"%B %d, %Y\",\"short\":\"%b %d\"},\"month_names\":[null,\"January\",\"February\",\"March\",\"April\",\"May\",\"June\",\"July\",\"August\",\"September\",\"October\",\"November\",\"December\"]},\"fallback_test\":\"Success\",\"foo\":\"Foo\",\"null_test\":\"fallback for null\",\"number\":{\"currency\":{\"format\":{\"delimiter\":\",\",\"format\":\"%u%n\",\"precision\":2,\"separator\":\".\",\"unit\":\"$\"}},\"format\":{\"delimiter\":\",\",\"precision\":3,\"separator\":\".\"}},\"merge_plurals\":{\"one\":\"Apple\",\"other\":\"Apples\"},\"time\":{\"am\":\"am\",\"formats\":{\"default\":\"%a, %d %b %Y %H:%M:%S %z\",\"long\":\"%B %d, %Y %H:%M\",\"short\":\"%d %b %H:%M\"},\"pm\":\"pm\"}});
 EOS
-)
         end
       end
     end
