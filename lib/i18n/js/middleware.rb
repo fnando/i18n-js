8fc9c0d212907f636dfbb375ca14e55f5d896d36
Hamed Asghari <hasghari@gmail.com>
2021-03-10 17:52:03 -0700

    Fix deprecation warning
    Fixes the deprecation warning "warning: File.exists? is deprecated; use File.exist? instead"

--- lib/i18n/js/middleware.rb
+++ lib/i18n/js/middleware.rb
@@ -45,7 +45,7 @@ module I18n
 
       def save_cache(new_cache)
         # path could be a symbolic link
-        FileUtils.mkdir_p(cache_dir) unless File.exists?(cache_dir)
+        FileUtils.mkdir_p(cache_dir) unless File.exist?(cache_dir)
         File.open(cache_path, "w+") do |file|
           file << new_cache.to_yaml
         end
