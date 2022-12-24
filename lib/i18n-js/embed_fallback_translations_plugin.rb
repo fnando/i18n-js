50fe3b5dd1a6e155d3ce98a80d7bc31f1a21d5ad
Nando Vieira <me@fnando.com>
2022-12-24 15:28:50 -0800

    Use an array for comparison.

--- lib/i18n-js/embed_fallback_translations_plugin.rb
+++ lib/i18n-js/embed_fallback_translations_plugin.rb
@@ -5,16 +5,19 @@ module I18nJS
 
   class EmbedFallbackTranslationsPlugin < I18nJS::Plugin
     module Utils
-      # Based on deep_merge by Stefan Rusterholz, see <https://www.ruby-forum.com/topic/142809>.
-      # This method is used to handle I18n fallbacks. Given two equivalent path nodes in two locale trees:
-      # 1. If the node in the current locale appears to be an I18n pluralization (:one, :other, etc.),
-      #    use the node, but merge in any missing/non-nil keys from the fallback (default) locale.
-      # 2. Else if both nodes are Hashes, combine (merge) the key-value pairs of the two nodes into one,
-      #    prioritizing the current locale.
+      # Based on deep_merge by Stefan Rusterholz, see
+      # <https://www.ruby-forum.com/topic/142809>.
+      # This method is used to handle I18n fallbacks. Given two equivalent path
+      # nodes in two locale trees:
+      # 1. If the node in the current locale appears to be an I18n pluralization
+      #    (:one, :other, etc.), use the node, but merge in any missing/non-nil
+      #    keys from the fallback (default) locale.
+      # 2. Else if both nodes are Hashes, combine (merge) the key-value pairs of
+      #    the two nodes into one, prioritizing the current locale.
       # 3. Else if either node is nil, use the other node.
 
       PLURAL_KEYS = %i[zero one two few many other].freeze
-      PLURAL_MERGER = proc { |_key, v1, v2| v1 || v2 }
+      PLURAL_MERGER = proc {|_key, v1, v2| v1 || v2 }
       MERGER = proc do |_key, v1, v2|
         if v1.is_a?(Hash) && v2.is_a?(Hash)
           if (v2.keys - PLURAL_KEYS).empty?
@@ -47,10 +50,11 @@ module I18nJS
       return translations unless enabled?
 
       fallback_locale = I18n.default_locale.to_sym
-      locales_to_fallback = translations.keys - fallback_locale
+      locales_to_fallback = translations.keys - [fallback_locale]
 
       translations_with_fallback = {}
-      translations_with_fallback[fallback_locale] = translations[fallback_locale]
+      translations_with_fallback[fallback_locale] =
+        translations[fallback_locale]
 
       locales_to_fallback.each do |locale|
         translations_with_fallback[locale] = Utils.deep_merge(
